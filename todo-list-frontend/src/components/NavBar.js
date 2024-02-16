246822c74cf91b814387c9810991ba1a640cdab9
TrevorHenry05 <trevorjhen03@gmail.com>
2024-02-16 15:53:43 -0800

    Added logout to navbar

--- todo-list-frontend/src/components/NavBar.js
+++ todo-list-frontend/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NavBar() {
+function NavBar({ onLogout }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="collapse navbar-collapse">
@@ -15,6 +15,11 @@ function NavBar() {
             <Link className="nav-link" to="/settings">
               Settings
             </Link>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link btn btn-light" onClick={onLogout}>
+              Logout
+            </button>
           </li>
         </ul>
       </div>
